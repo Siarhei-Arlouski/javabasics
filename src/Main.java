e9b9bc4055ffe7fb36548a4f0e05adf98525abe4
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-10-28 13:18:46 +0300

    Added 1-4 tasks

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {                //условные операторы - задание 1
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int x = scanner.nextInt();
+        if (x > 10) {
+            System.out.println("Число больше десяти");
+        } else if (x < 10) {
+            System.out.println("Число меньше десяти");
+        }
+    taskTwo();
+    taskThree();
+    taskFour();*/
+    taskFive();
+    }
+    /*public static void taskTwo() {                          //условные операторы - задание 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int x = scanner.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Введите второе число: ");
+        int x2 = scanner2.nextInt();
+        if (x > x2) {
+            System.out.println("Первое число больше, чем второе");
+        } else if (x < x2) {
+            System.out.println("Первое число меньше, чем второе");
+        }
+    }
+    public static void taskThree() {                        //условные операторы - задание 3
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину первой стороны треугольника: ");
+        int x = scanner.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Введите длину второй стороны треугольника: ");
+        int x2 = scanner2.nextInt();
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Введите длину третьей стороны треугольника: ");
+        int x3 = scanner3.nextInt();
+        if (x == x2 & x == x3) {
+            System.out.println("Треугольник является равносторонним");
+        } else if (x == x2 || x == x3 || x2 == x3) {
+            System.out.println("Треугольник является равнобедренным");
+        } else {
+            System.out.println("Треугольник является разносторонним");
+        }
+    }
+    public static void taskFour() {                           //условные операторы- задание 4
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int x = scanner.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Введите второе число: ");
+        int x2 = scanner2.nextInt();
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Введите третье число: ");
+        int x3 = scanner3.nextInt();
+        Scanner scanner4 = new Scanner(System.in);
+        System.out.println("Введите четвертое число: ");
+        int x4 = scanner4.nextInt();
+        int positive = 0;
+        int negative = 0;
+        if (x > 0) positive++;
+        if (x < 0) negative++;
+        if (x2 > 0) positive++;
+        if (x2 < 0) negative++;
+        if (x3 > 0) positive++;
+        if (x3 < 0) negative++;
+        if (x4 > 0) positive++;
+        if (x4 < 0) negative++;
+        System.out.println("Количество положительных чисел: " + positive);
+        System.out.println("Количество отрицательных чисел: " + negative);
+    }*/
+    public static void taskFive() {
+        
+    }
+}
